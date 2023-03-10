89042ab76bc25bc279e795c13127cd7650d54636
Sebastian <sebastian.franz@tum.de>
2023-03-10 17:02:21 +0100

    Adding splits generators
    Classes to create dataset splits from standardized human-virus interaction datasets (DatasetHVIStandardized).
    Train/Val/Test splits are generated and written to a biotrainer-readable format. It can then be used with the interaction mode of biotrainer.

---
+++ hvi_datasets/dataset_splitting/__init__.py
@@ -0,0 +1,8 @@
+from .splits_generators import SplitsGenerator, SplitGeneratorRandomHoldOut, SplitGeneratorCrossValidation, Interaction
+
+__all__ = [
+    "SplitsGenerator",
+    "SplitGeneratorRandomHoldOut",
+    "SplitGeneratorCrossValidation",
+    "Interaction"
+]
