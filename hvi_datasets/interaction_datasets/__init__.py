9b9b2b581af623d172f90e1c0d0dd1e69e050ddd
Sebastian <sebastian.franz@tum.de>
2023-03-22 10:59:53 +0100

    Adding HVI Dataset for rabies lyssa experimental interactions

--- hvi_datasets/interaction_datasets/__init__.py
+++ hvi_datasets/interaction_datasets/__init__.py
@@ -6,6 +6,7 @@ from .vir_host_net_3 import DatasetVirHostNet3
 from .viruses_string import DatasetVirusesString
 from .sars_cov_2_im_28880 import DatasetSarsCov2IM28880
 from .tsukiyama_negatives import DatasetTsukiyamaNegatives
+from .rabies_lyssavirus_experimental import DatasetRabiesLyssavirusExperimental
 
 __all__ = [
     "DatasetHVIDB",
@@ -15,5 +16,6 @@ __all__ = [
     "DatasetVirHostNet3",
     "DatasetVirusesString",
     "DatasetSarsCov2IM28880",
-    "DatasetTsukiyamaNegatives"
+    "DatasetTsukiyamaNegatives",
+    "DatasetRabiesLyssavirusExperimental"
 ]
